e191780ab5a1b199089db7d41282f795b668cc4c
Marta Dychała <mar754dyc@gmail.com>
2024-12-13 02:44:49 +0100

    Configure Dockerfiles

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(process.env.FRONTEND_BASE_URL);
   app.enableCors({
     origin: process.env.FRONTEND_BASE_URL,
     credentials: true,
